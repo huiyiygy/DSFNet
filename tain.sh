c7498306c0e6507a6638f3275742d8e63a014c30
huiyiygy <huiyiygy@aliyun.com>
2019-11-22 14:37:28 +0800

    update train.sh

--- tain.sh
+++ tain.sh
@@ -4,4 +4,10 @@
 # CUDA_VISIBLE_DEVICES=0 python train.py --lr 0.001 --weight-decay 0.0 --out-stride 8 --epochs 400 --batch-size 8 --base-size 512 --crop-size 512 --gpu-ids 0 --checkname dsfnet --eval-interval 1
 
 # experiment 1 native DSFNet use-balanced-weights out-stride 16
-CUDA_VISIBLE_DEVICES=0 python train.py --lr 0.001 --weight-decay 0.0 --use-balanced-weights --out-stride 16 --epochs 400 --batch-size 12 --base-size 512 --crop-size 512 --gpu-ids 0 --checkname dsfnet --eval-interval 1
+# CUDA_VISIBLE_DEVICES=0 python train.py --lr 0.001 --weight-decay 0.0 --use-balanced-weights --out-stride 16 --epochs 400 --batch-size 12 --base-size 512 --crop-size 512 --gpu-ids 0 --checkname dsfnet --eval-interval 1
+
+# experiment 2 native DSFNet use-balanced-weights out-stride 8
+# CUDA_VISIBLE_DEVICES=0 python train.py --lr 0.001 --weight-decay 0.0 --use-balanced-weights --out-stride 8 --epochs 400 --batch-size 8 --base-size 512 --crop-size 512 --gpu-ids 0 --checkname dsfnet --eval-interval 1
+
+# experiment 3 native DSFNet out-stride 16
+CUDA_VISIBLE_DEVICES=0 python train.py --lr 0.001 --weight-decay 0.0 --out-stride 16 --epochs 400 --batch-size 12 --base-size 512 --crop-size 512 --gpu-ids 0 --checkname dsfnet --eval-interval 1
