ef0ded63cd60cb102166da77c2d43d4bd06fcc7e
huiyiygy <huiyiygy@aliyun.com>
2019-11-27 16:49:29 +0800

    update train.sh

--- tain.sh
+++ tain.sh
@@ -1,14 +1,25 @@
 #!/usr/bin/env bash
 
 # experiment 0 native DSFNet out-stride 8
-CUDA_VISIBLE_DEVICES=0 python train.py --lr 0.001 --out-stride 8 --epochs 400 --batch-size 8 --base-size 512 --crop-size 512 --gpu-ids 0 --checkname dsfnet --eval-interval 1
+# CUDA_VISIBLE_DEVICES=0 python train.py --lr 0.001 --out-stride 8 --epochs 300 --batch-size 8 --base-size 512 --crop-size 512 --gpu-ids 0 --checkname dsfnet --eval-interval 1
 
 # experiment 1 native DSFNet use-balanced-weights out-stride 8
-CUDA_VISIBLE_DEVICES=0 python train.py --lr 0.001 --use-balanced-weights --out-stride 8 --epochs 400 --batch-size 8 --base-size 512 --crop-size 512 --gpu-ids 0 --checkname dsfnet --eval-interval 1
+CUDA_VISIBLE_DEVICES=0 python train.py --lr 0.001 --use-balanced-weights --out-stride 8 --epochs 300 --batch-size 8 --base-size 512 --crop-size 512 --gpu-ids 0 --checkname dsfnet --eval-interval 1
 
 # experiment 2 native DSFNet out-stride 16
-# CUDA_VISIBLE_DEVICES=0 python train.py --lr 0.001 --out-stride 16 --epochs 400 --batch-size 16 --base-size 512 --crop-size 512 --gpu-ids 0 --checkname dsfnet --eval-interval 1
+# CUDA_VISIBLE_DEVICES=0 python train.py --lr 0.001 --out-stride 16 --epochs 300 --batch-size 16 --base-size 512 --crop-size 512 --gpu-ids 0 --checkname dsfnet --eval-interval 1
 
 # experiment 3 native DSFNet use-balanced-weights out-stride 16
-# CUDA_VISIBLE_DEVICES=0 python train.py --lr 0.001 --use-balanced-weights --out-stride 16 --epochs 400 --batch-size 16 --base-size 512 --crop-size 512 --gpu-ids 0 --checkname dsfnet --eval-interval 1
+# CUDA_VISIBLE_DEVICES=0 python train.py --lr 0.001 --use-balanced-weights --out-stride 16 --epochs 300 --batch-size 16 --base-size 512 --crop-size 512 --gpu-ids 0 --checkname dsfnet --eval-interval 1
 
+# experiment 4 attention DSFNet out-stride 8
+# CUDA_VISIBLE_DEVICES=0 python train.py --lr 0.001 --use-attention --out-stride 8 --epochs 300 --batch-size 8 --base-size 512 --crop-size 512 --gpu-ids 0 --checkname dsfnet --eval-interval 1
+
+# experiment 5 attention DSFNet use-balanced-weights out-stride 8
+# CUDA_VISIBLE_DEVICES=0 python train.py --lr 0.001 --use-balanced-weights --use-attention --out-stride 8 --epochs 300 --batch-size 8 --base-size 512 --crop-size 512 --gpu-ids 0 --checkname dsfnet --eval-interval 1
+
+# experiment 6 attention DSFNet out-stride 16
+# CUDA_VISIBLE_DEVICES=0 python train.py --lr 0.001 --use-attention --out-stride 16 --epochs 300 --batch-size 16 --base-size 512 --crop-size 512 --gpu-ids 0 --checkname dsfnet --eval-interval 1
+
+# experiment 7 attention DSFNet use-balanced-weights out-stride 16
+# CUDA_VISIBLE_DEVICES=0 python train.py --lr 0.001 --use-balanced-weights --use-attention --out-stride 16 --epochs 300 --batch-size 16 --base-size 512 --crop-size 512 --gpu-ids 0 --checkname dsfnet --eval-interval 1
