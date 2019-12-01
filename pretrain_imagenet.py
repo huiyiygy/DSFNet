a642c490f354bb7fb21712e5b858f2e703449a8e
huiyiygy <huiyiygy@aliyun.com>
2019-12-01 17:14:49 +0800

    修改ImageNet代码

--- pretrain_imagenet.py
+++ pretrain_imagenet.py
@@ -7,61 +7,55 @@ import time
 
 import torch
 import torch.nn as nn
-import torch.nn.parallel
-import torch.backends.cudnn as cudnn
 import torch.optim
 import torch.utils.data
-import torch.utils.data.distributed
 import torchvision.transforms as transforms
 import torchvision.datasets as datasets
 
 from model.xception_imagenet_pretrain import XceptionClassifier
 
 
-parser = argparse.ArgumentParser(description='PyTorch ImageNet Training')
-parser.add_argument('--data',  default='/home/lab/ygy/dataset/ImageNet', type=str,
-                    help='path to dataset')
-parser.add_argument('-j', '--workers', default=4, type=int, metavar='N',
-                    help='number of data loading workers (default: 4)')
-parser.add_argument('--epochs', default=90, type=int, metavar='N',
-                    help='number of total epochs to run')
-parser.add_argument('--start-epoch', default=0, type=int, metavar='N',
-                    help='manual epoch number (useful on restarts)')
-parser.add_argument('-b', '--batch-size', default=256, type=int, metavar='N',
-                    help='mini-batch size (default: 256), this is the total '
-                         'batch size of all GPUs on the current node when '
-                         'using Data Parallel or Distributed Data Parallel')
-parser.add_argument('--lr', '--learning-rate', default=0.1, type=float,
-                    metavar='LR', help='initial learning rate', dest='lr')
-parser.add_argument('--momentum', default=0.9, type=float, metavar='M',
-                    help='momentum')
-parser.add_argument('--wd', '--weight-decay', default=1e-4, type=float,
-                    metavar='W', help='weight decay (default: 1e-4)',
-                    dest='weight_decay')
-parser.add_argument('-p', '--print-freq', default=10, type=int,
-                    metavar='N', help='print frequency (default: 10)')
-parser.add_argument('--seed', type=int, default=1, metavar='S', help='random seed (default: 1)')
-parser.add_argument('--resume', default='', type=str, metavar='PATH',
-                    help='path to latest checkpoint (default: none)')
-parser.add_argument('--gpu', default=None, type=int, help='GPU id to use.')
-
-best_acc1 = 0
-
-
 def main():
+    parser = argparse.ArgumentParser(description='PyTorch ImageNet Training')
+    parser.add_argument('--data', default='/home/lab/ygy/dataset/ImageNet', type=str,
+                        help='path to dataset')
+    parser.add_argument('-j', '--workers', default=4, type=int, metavar='N',
+                        help='number of data loading workers (default: 4)')
+    parser.add_argument('--epochs', default=90, type=int, metavar='N',
+                        help='number of total epochs to run')
+    parser.add_argument('--start-epoch', default=0, type=int, metavar='N',
+                        help='manual epoch number (useful on restarts)')
+    parser.add_argument('-b', '--batch-size', default=256, type=int, metavar='N',
+                        help='mini-batch size (default: 256), this is the total '
+                             'batch size of all GPUs on the current node when '
+                             'using Data Parallel or Distributed Data Parallel')
+    parser.add_argument('--lr', '--learning-rate', default=0.1, type=float,
+                        metavar='LR', help='initial learning rate', dest='lr')
+    parser.add_argument('--momentum', default=0.9, type=float, metavar='M',
+                        help='momentum')
+    parser.add_argument('--wd', '--weight-decay', default=1e-4, type=float,
+                        metavar='W', help='weight decay (default: 1e-4)',
+                        dest='weight_decay')
+    parser.add_argument('-p', '--print-freq', default=10, type=int,
+                        metavar='N', help='print frequency (default: 10)')
+    parser.add_argument('--seed', type=int, default=1, metavar='S', help='random seed (default: 1)')
+    parser.add_argument('--resume', default='', type=str, metavar='PATH',
+                        help='path to latest checkpoint (default: none)')
+    parser.add_argument('--gpu', default=None, type=int, help='GPU id to use.')
+
     args = parser.parse_args()
 
     random.seed(args.seed)
     torch.manual_seed(args.seed)
-    cudnn.deterministic = True
-    cudnn.benchmark = True
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = True
 
     # Simply call main_worker function
     main_worker(args.gpu, args)
 
 
 def main_worker(gpu,  args):
-    global best_acc1
+    best_acc1 = 0.0
     args.gpu = gpu
 
     if args.gpu is not None:
--- pretrain_imagenet.sh
+++ pretrain_imagenet.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-CUDA_VISIBLE_DEVICES=0 python pretrain_imagenet.py --epochs 90 -b 40 --lr 0.1 --gpu 0 -p 50
+CUDA_VISIBLE_DEVICES=0 python pretrain_imagenet.py --epochs 90 -b 40 --lr 0.1 --gpu 0
